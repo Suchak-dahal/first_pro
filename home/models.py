b40e9d02e8ab40535f17202939221c820c21a034
Suchak Dahal <sharmasupreme111@gmail.com>
2023-12-27 17:08:22 +0545

    new templates

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Slider(models.Model):
+    title = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='media')
+    url = models.URLField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
